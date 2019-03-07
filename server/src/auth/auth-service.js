3d4536a6ea6c5e2b80dd15e5fdcd7eb03337a7cc
Payman Kossari <Paymankossari@gmail.com>
2019-03-07 12:47:56 -0500

    registratino complete

--- server/src/auth/auth-service.js
+++ server/src/auth/auth-service.js
@@ -1,26 +1,28 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
- const AuthService = {
-   getUserWithUserName(db, user_name) {
-     return db('blogful_users')
-       .where({ user_name })
-       .first()
-   },
+const AuthService = {
+  getUserWithUserName(db, user_name) {
+    return db('thingful_users')
+      .where({ user_name })
+      .first();
+  },
   comparePasswords(password, hash) {
-    return bcrypt.compare(password, hash)
+    return bcrypt.compare(password, hash);
   },
   createJwt(subject, payload) {
     return jwt.sign(payload, config.JWT_SECRET, {
       subject,
-      algorithm: 'HS256',
-    })
+      algorithm: 'HS256'
+    });
   },
-  verifyJwt(token){
+  verifyJwt(token) {
     return jwt.verify(token, config.JWT_SECRET, {
       algorithms: ['HS256']
-    })
+    });
   },
   parseBasicToken(token) {}
-}
+};
+
+module.exports = AuthService;
